8c5b426ed56e8fcb63750cb4ca7fcfb00a7b49d0
Peter Gottesman <peter@petergottesman.com>
2018-02-28 10:30:44 -0500

    Modified install.sh to install from a local copy

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-wget -O spotifycli https://raw.githubusercontent.com/pwittchen/spotify-cli-linux/master/spotifycli
 chmod +x spotifycli
-sudo mv spotifycli /usr/local/bin/spotifycli
+mv spotifycli /usr/local/bin/spotifycli
+mv spotifycli /usr/local/bin/sp
